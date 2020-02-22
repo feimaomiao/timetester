9beed6e29a189616ac411988023fd98985117840
feimaomiao <lcpmatthew@gmail.com>
2020-02-21 17:08:41 -0800

    updated functions

--- test_time/__init__.py
+++ test_time/__init__.py
@@ -4,7 +4,7 @@ import signal
 from functools import wraps
 from errno import ETIME
 import os
-from decimals import Decimals as dec
+from decimal import Decimal as dec
 
 class timeTesterError(Exception):
     pass
@@ -29,7 +29,7 @@ def timeout(seconds=10, error_message=os.strerror(ETIME)):
     return decorator
 
 class tester():
-    def __init__(self, method ,target=10,runtime=100, maxtime=100, error_time=100, return_type = 'mean'):
+    def __init__(self, method ,target=100000,runtime=100, maxtime=100, error_time=100, return_type = 'mean'):
         self.function   = method
         try:
             self.runtime        = int(runtime)
@@ -38,12 +38,15 @@ class tester():
             self.error_time     = int(error_time)
             self.__average      = []
             self.type           = return_type
-            self.__maxallowance = self.runtime*self.error_time
+            self.__errorEncd    = 0
+            self.__totalruntime = 0
         except TypeError:
-            print('runtime, maxtime and errortime must be ints')
+            print('runtime, maxtime and errortime must be int')
             raise
         if runtime<=0 or target<=0 or maxtime<=0 or error_time<=0:
             raise timeTesterError('runtime, target, maxtime and errortime cannot be smaller than 0')
+        if maxtime<self.error_time:
+            raise timeTesterError('maxtime must be larger than error time!')
 
     def __repr__(self):
         try:
@@ -52,6 +55,7 @@ class tester():
             elif self.type == 'median':
                 return str(dec(st.median(self.__average)))
             else:
+                self.type = 'mean'
                 return str(dec(st.mean(self.__average)))
         except st.StatisticsError as e:
             return 'Run a time test before getting its time!'
@@ -59,18 +63,49 @@ class tester():
             return 'An error occured'
 
     def run_tests(self, *args, **kwargs):
+        def __raise_error(signum, frame):
+            raise TimeoutError(f'Testrun #{i} took longer than selected time to respond.')
+
         beginning_time = time.time()
         for i in range(self.runtime):
             starttime= time.time()
+            signal.signal(signal.SIGALRM, __raise_error)
             try:
+                signal.alarm(self.error_time)
                 self.function(*args,**kwargs)
+                signal.alarm(0)
+            except TimeoutError:
+                self.__errorEncd += 1
+                raise
             except Exception as e:
-                raise timeTesterError('Your program ran into a problem,', e)
+                self.__errorEncd += 1
+                signal.alarm(0)
+                raise
             run_time = time.time()-starttime
             if run_time >= self.maxtime:
-                raise os.strerror(ETIME)
+                self.__errorEncd
+                raise TimeoutError('Tests took longer than expected!')
             self.__average.append(run_time)
             del starttime, run_time
+        self.__totalruntime += (beginning_time - time.time())
+
+    def report(self):
+        returnString = f'''\
+Expected runs       : {self.runtime}
+Target              : {self.target}
+Maximum time        : {self.maxtime}(Total), {self.error_time}(Single run)
+Successful runs     : {len(self.__average)}
+Total errors        : {self.__errorEncd}
+repr type           : {self.type}
+Mean time           : {str(dec(st.mean(self.__average)))}
+Median time         : {str(dec(st.median(self.__average)))}
+Mode time           : {str(dec(st.median(self.__average)))}
+Harmonic mean time  : {str(dec(st.harmonic_mean(self.__average)))}
+Meeting Target      : {True if dec(st.mean(self.__average))< self.target else False}
+To target(mean)     : {abs(self.target-dec(st.mean(self.__average)))}
+'''
+        return returnString
+
 
 
 
