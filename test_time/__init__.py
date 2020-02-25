27447c776314733fdfff9f2584fd1b53844249d6
feimaomiao <lcpmatthew@gmail.com>
2020-02-25 00:06:07 -0800

    updated plot method

--- test_time/__init__.py
+++ test_time/__init__.py
@@ -5,14 +5,19 @@ from functools import wraps
 from errno import ETIME
 import os
 from decimal import Decimal as dec
+import random, string
+class timeTesterError(Exception):
+    pass
 
 def test_function_delete_later():
-    import random, string
     password_characters = string.ascii_letters + string.digits
     return ''.join(random.choice(password_characters) for i in range(random.randrange(15,20)))
 
-class timeTesterError(Exception):
-    pass
+def test_function_2():
+    if random.choices([True, False], weights=[0.2,0.8])[0]:
+        raise timeTesterError
+    return
+
 
 def timeout(seconds=10, error_message=os.strerror(ETIME)):
     def decorator(func):
@@ -34,7 +39,7 @@ def timeout(seconds=10, error_message=os.strerror(ETIME)):
     return decorator
 
 class tester():
-    def __init__(self, method ,target=100000,runtime=100, maxtime=100, error_time=100, return_type = 'mean'):
+    def __init__(self, method ,target=1,runtime=100, maxtime=10, error_time=0, return_type = 'mean'):
         self.function   = method
         try:
             self.runtime        = int(runtime)
@@ -51,7 +56,7 @@ class tester():
         except TypeError:
             print('runtime, maxtime and errortime must be int')
             raise
-        if runtime<=0 or target<=0 or maxtime<=0 or error_time<0:
+        if runtime<=0 or target<=0 or maxtime<0 or error_time<0:
             raise timeTesterError('runtime, target, maxtime and errortime cannot be smaller than 0')
         if maxtime<self.error_time:
             raise timeTesterError('maxtime must be larger than error time!')
@@ -92,7 +97,7 @@ class tester():
                 raise
             __runtime_o = dec(time.time()-__starttime)
             if __runtime_o >= self.maxtime:
-                self.__errorEncd
+                self.__errorEncd += 1
                 raise TimeoutError('Tests took longer than expected!')
             self.__average.append(__runtime_o)
             del __starttime, __runtime_o
@@ -105,7 +110,19 @@ class tester():
         except ImportError:
             print('Please make sure you have matplotlib module downloaded from pip!')
             return
-        plt.plot([x for x in range(len(self.__average))], self.__average, color = 'darkblue', linewidth=1)
+        if self.__errorEncd >0:
+            raise timeTesterError('Please make sure no error is encountered before actually showing the test time')
+        plt.plot([x for x in range(len(self.__average))], self.__average, color = 'darkblue', linewidth=1, label="Actual time")
+        actualaverage = st.mode(self.__average) if self.type == 'mode' else st.median(self.__average) if self.type == 'median' else st.mean(self.__average)
+        plt.plot([x for x in range(len(self.__average))], [actualaverage for x in range(len(self.__average))], color='green',linewidth=1, label="average")
+        plt.legend()
+        plt.xlabel('Times')
+        plt.ylabel('Seconds')
+        ylimit = 1.5*float(self.target) if self.target<max(self.__average) else float(max(self.__average)) if max(self.__average)< st.median(self.__average) * 3 else float(st.median(self.__average)) * 3
+        plt.plot([x for x in range(len(self.__average))], [self.target for x in range(len(self.__average))], color='red', linewidth=1, label="target time") if self.target<= ylimit else None
+        print(ylimit)
+        plt.ylim(0, ylimit)
+        plt.title('Average time')
         plt.show()
 
 
@@ -124,8 +141,8 @@ Mode time           : {str(dec(st.median(self.__average)))}
 Mode time appearance: {self.__average.count(dec(st.median(self.__average)))}
 Harmonic mean time  : {str(dec(st.harmonic_mean(self.__average)))}
 Meeting Target      : {True if dec(st.mean(self.__average))< self.target else False}
-To target(mean)(abs): {-abs(self.target-dec(st.mean(self.__average)))}
-To target           : {self.target-dec(st.mean(self.__average))}
+To target(mean)(abs): {abs(self.target-dec(st.mean(self.__average)))}
+To target           : {-(self.target-dec(st.mean(self.__average)))}
 Max time in run     : {max(self.__average)}
 Max time index      : {self.__average.index(max(self.__average))}
 Max time appearance : {self.__average.count(max(self.__average))}
